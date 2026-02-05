c1b407c9a8b7dfca32f85417a94ceb9d92fe1942
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-02-06 00:00:38 +0530

    files

---
+++ ass1.java
@@ -0,0 +1,33 @@
+/*
+Q1. Write a Java program to create a text file named info.txt and write the message "Welcome to Java File Handling" into it.
+Explanation
+This program helps you understand how to create a file and write data into it using Java file handling classes.
+What to Do :- 
+Create a file
+Write a simple text message
+Close the file properly
+Expected Learning :- 
+File class
+FileWriter class
+now we'll do some assignment on file handling
+*/
+import java.io.*;
+import java.util.*;
+
+public class ass1{
+	public static void main(String ...x)throws Exception{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the data in the file");
+		File f= new File("E:\\downloads\\file handling giris\\info.txt");
+		FileWriter fw = new FileWriter(f);
+		
+		fw.write(sc.nextLine());
+		fw.close();
+		FileReader fr = new FileReader(f);
+		int data;
+		while((data = fr.read()) != -1){
+			System.out.print((char)data);
+		}
+		
+	}
+}
---
+++ ass2.java
@@ -0,0 +1,26 @@
+/*
+Q2. Write a Java program to read and display all content from the file info.txt.
+Explanation
+This program teaches how to open an existing file and read data from it line by line.
+What to Do :- 
+Open the file
+Read the content
+Print it on the console
+Expected Learning :-
+FileReader
+BufferedReader
+
+*/
+import java.io.*;
+import java.util.*;
+
+public class ass2{
+	public static void main(String ...x){
+		System.out.println("Enter the data");
+		
+		File f = new File("E:\downloads\file handling giris\\ByCode\\info.txt");
+		Reader r = new FileReader(f);
+		
+		
+	}
+}
