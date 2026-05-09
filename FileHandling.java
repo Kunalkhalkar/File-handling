b1f3ed51eb63b4bc9c44e5f241923c89a2cdbd2f
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-05-09 23:45:35 +0500

    buffered reader

--- FileHandling.java
+++ FileHandling.java
@@ -4,5 +4,7 @@ Q6. Create file using a BufferedWriter class and store string data in it?
 
 import java.util.*;
 public class Practice{
-	public static void main(String...x)
+public static void main(String...x){
+	Scanner sc = new Scanner(System.in);
+}
 }
